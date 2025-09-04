6112f7f94af9c7bb1c4220234715f6e637a4d2d9
aminandishe <maziarati8@gmail.com>
2025-09-05 00:14:30 +0330

    fix: remove duplicate GamePlayPlayerModel.cs class

--- Assets/Scripts/GamePlay/Models/Players/GamePlayPlayerModel.cs
+++ Assets/Scripts/GamePlay/Models/Players/GamePlayPlayerModel.cs
@@ -1,16 +1,37 @@
 using Core.Models.Players;
+using GamePlay.Components;
+using GamePlay.Components.Abstractions;
+using GamePlay.Configs;
 using GamePlay.Models.Abstractions;
 using Zenject;
 
-namespace GamePlay.Models.Players
+namespace GamePlay.Models
 {
     public class GamePlayPlayerModel : GamePlayBaseModel
     {
-        
         [Inject]
-        public void Construct(CorePlayerModel corePlayerModel)
+        public void Construct(CorePlayerModel corePlayerModel, PlayerConfig playerConfig)
         {
             CoreBaseModel = corePlayerModel;
+
+            AddComponents();
+            InitConfig(playerConfig);
+        }
+
+        private void AddComponents()
+        {
+            var components = GetComponents<GamePlayBaseComponent>();
+            foreach (var component in components)
+            {
+                var coreComponent = component.GetCoreBaseComponent();
+                CoreBaseModel.AddComponent(coreComponent);
+            }
+        }
+
+        private void InitConfig(PlayerConfig playerConfig)
+        {
+            var movementComponent = GetComponent<GamePlayMovementComponent>();
+            movementComponent?.SetMovementSpeed(playerConfig.MovementSpeed);
         }
     }
 }
